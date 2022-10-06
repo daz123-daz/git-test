599876df0df037e4518e4b471cfa9ecf547c0856
daz <daz@163.com>
2022-10-06 11:09:52 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @author ：daz
+ * @date ：Created in 2022/10/6 11:06
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
